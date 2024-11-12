e62961d949af83b0eb164ee8e29fe19df5eec19b
matheus rodrigues lima <matheusroma.ctt@gmail.com>
2024-11-12 16:17:56 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,12 @@
 package application;
 
-import model.entities.Department;
-import model.entities.Seller;
-
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -26,12 +26,16 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("=== TEST 2: seller findAll ====");
+		System.out.println("=== TEST 3: seller findAll ====");
 		list = sellerDao.findAll();
 		for(Seller obj :list) {
 			System.out.println(obj);
 		}
 		
+		System.out.println("=== TEST 4: seller insert ====");
+		Seller newSeller = new Seller(null,"Greg","greg@gmail.com",new Date(), 4000.0 , department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }
